65bbdcf1858684beb522449e229ccebbb5e470bf
Samu-GGH <167037572+Samu-GGH@users.noreply.github.com>
2025-06-09 13:20:05 -0400

    agregar ranking

---
+++ src/app/components/ranking.tsx
@@ -0,0 +1,214 @@
+"use client";
+//import Link from "next/link";
+import Image from "next/image";
+
+const Ranking = () => {
+  return (
+    <div className="relative inline-flex flex-wrap BGCcomponent justify-center w-full mt-3 overflow-hidden">
+      <Image
+        src="\volleyball-ball-svgrepo-com-white.png"
+        width={0}
+        height={0}
+        alt={""}
+        className="w-full object-cover absolute top-40 -left-20 opacity-10 z-100"
+      />
+      <div className="rank-sub9 relative w-[90%] lg:w-[70%] rounded-2xl p-3 bg-red-950 my-3">
+        <span className="block relative text-center text-white text-xl md:text-2xl lg:text-4xl font-extrabold mt-3">
+          RANKING
+        </span>
+        <span className="block relative text-center text-white text-sm md:text-md lg:text-lg font-medium mb-3">
+          SUB-9
+        </span>
+        <div className="relative inline-flex w-full">
+          <table className="table-fixed w-full text-white md:text-lg lg:text-2xl">
+            <thead>
+              <tr className="border-b-3 border-white">
+                <th className="w-[10%] py-1.5">Pos.</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Equipo</th>
+                <th className="w-[10%] py-1.5">G</th>
+                <th className="w-[10%] py-1.5">P</th>
+                <th className="w-[10%] py-1.5">Pts</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th className="w-[10%] pt-6 ">1</th>
+                <th className="w-[60%] pt-6 text-left pl-10">Mejias</th>
+                <th className="w-[10%] pt-6">6</th>
+                <th className="w-[10%] pt-6">2</th>
+                <th className="w-[10%] pt-6">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="rank-sub9 relative w-[90%] lg:w-[70%] rounded-2xl p-3 bg-red-950 my-3">
+        <span className="block relative text-center text-white text-xl md:text-2xl lg:text-4xl font-extrabold mt-3">
+          RANKING
+        </span>
+        <span className="block relative text-center text-white text-sm md:text-md lg:text-lg font-medium mb-3">
+          SUB-9
+        </span>
+        <div className="relative inline-flex w-full">
+          <table className="table-fixed w-full text-white md:text-lg lg:text-2xl">
+            <thead>
+              <tr className="border-b-3 border-white">
+                <th className="w-[10%] py-1.5">Pos.</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Equipo</th>
+                <th className="w-[10%] py-1.5">G</th>
+                <th className="w-[10%] py-1.5">P</th>
+                <th className="w-[10%] py-1.5">Pts</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th className="w-[10%] pt-6 ">1</th>
+                <th className="w-[60%] pt-6 text-left pl-10">Mejias</th>
+                <th className="w-[10%] pt-6">6</th>
+                <th className="w-[10%] pt-6">2</th>
+                <th className="w-[10%] pt-6">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="rank-sub9 relative w-[90%] lg:w-[70%] rounded-2xl p-3 bg-red-950 my-3">
+        <span className="block relative text-center text-white text-xl md:text-2xl lg:text-4xl font-extrabold mt-3">
+          RANKING
+        </span>
+        <span className="block relative text-center text-white text-sm md:text-md lg:text-lg font-medium mb-3">
+          SUB-9
+        </span>
+        <div className="relative inline-flex w-full">
+          <table className="table-fixed w-full text-white md:text-lg lg:text-2xl">
+            <thead>
+              <tr className="border-b-3 border-white">
+                <th className="w-[10%] py-1.5">Pos.</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Equipo</th>
+                <th className="w-[10%] py-1.5">G</th>
+                <th className="w-[10%] py-1.5">P</th>
+                <th className="w-[10%] py-1.5">Pts</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th className="w-[10%] pt-6 ">1</th>
+                <th className="w-[60%] pt-6 text-left pl-10">Mejias</th>
+                <th className="w-[10%] pt-6">6</th>
+                <th className="w-[10%] pt-6">2</th>
+                <th className="w-[10%] pt-6">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+              <tr>
+                <th className="w-[10%] py-1.5">1</th>
+                <th className="w-[60%] py-1.5 text-left pl-10">Mejias</th>
+                <th className="w-[10%] py-1.5">2</th>
+                <th className="w-[10%] py-1.5">6</th>
+                <th className="w-[10%] py-1.5">18</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ranking;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/footer";
 import Carrousel from "./components/carrousel";
 import Calender from "./components/calender";
 import Newscontainer from "./components/noticiasContainer";
+import Ranking from "./components/ranking";
 
 const home = () => {
   return (
@@ -35,6 +36,24 @@ const home = () => {
           </span>
           <Newscontainer />
           <div className="masNoticias absolute inline-flex flex-wrap items-center justify-center w-25 p-1 text-[12px] md:text-[16px] -bottom-12 border-black border-2 rounded-2xl right-[5%] lg:right-[50%] lg:translate-x-[50%] cursor-pointer font-bold hover:bg-[#f11515] hover:text-white hover:border-red-950 transition-all duration-150 ease-out">
+            <span className="relative w-fit">Mas</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="chevron size-6 absolute right-2"
+            >
+              <path d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+            </svg>
+          </div>
+        </div>
+        <div className="rankingContainer relative block mt-3 mb-15">
+          <span className="relative block pl-4 pr-4 md:pl-12 md:pr-12 lg:pl-18 lg:pr-18 text-[16px] lg:text-[24px] font-bold">
+            Rankings
+          </span>
+          <Ranking />
+          <div className="masRankings absolute inline-flex flex-wrap items-center justify-center w-25 p-1 text-[12px] md:text-[16px] -bottom-12 border-black border-2 rounded-2xl right-[5%] lg:right-[50%] lg:translate-x-[50%] cursor-pointer font-bold hover:bg-[#f11515] hover:text-white hover:border-red-950 transition-all duration-150 ease-out">
             <span className="relative w-fit">Mas</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
